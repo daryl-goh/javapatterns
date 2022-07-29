528571873d35488640ccb11c00db4e46291e4a45
daryl-goh <goh.daryl@gmail.com>
2022-07-29 21:36:17 +0800

    added Pattern 2 and Pattern 3

--- Pattern2.java
+++ Pattern2.java
@@ -1,10 +1,11 @@
-public class Pattern1 {
+public class Pattern2 {
+
     public static void main(String[] args) {
         pattern1(5);
     }
     static void pattern1(int n) {
         for (int row = 1; row <= n; row++) {
-            for (int col = 1; col <= n; col++) {
+            for (int col = 1; col <= row; col++) {
                 System.out.print("* ");
             }
             System.out.println();
--- Pattern3.java
+++ Pattern3.java
@@ -1,10 +1,11 @@
-public class Pattern1 {
+public class Pattern3 {
+    
     public static void main(String[] args) {
         pattern1(5);
     }
     static void pattern1(int n) {
         for (int row = 1; row <= n; row++) {
-            for (int col = 1; col <= n; col++) {
+            for (int col = 1; col <= (n - row + 1); col++) {
                 System.out.print("* ");
             }
             System.out.println();
